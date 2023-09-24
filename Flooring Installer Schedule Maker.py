557bf9b05082b6dde7a44b5ae5ec144cf0e63581
Pedro Ayala <pedroayala120pa@gmail.com>
2023-09-23 17:00:21 -0700

    Update Flooring Installer Schedule Maker.py

--- Flooring Installer Schedule Maker.py
+++ Flooring Installer Schedule Maker.py
@@ -1,7 +1,7 @@
 # This program will help schedule a user inputted list of flooring installers, calculate the estimated time, and
 # assign the appropriate installer for each job
 # Pedro Ayala
-     
+      
 def main():  # main function
     # Create a dictionary to store the estimated time for each flooring type
     job_times = {
